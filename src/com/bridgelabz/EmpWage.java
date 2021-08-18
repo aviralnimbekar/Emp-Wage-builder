5166594ba8c1fad3680af32d478467ab77ed7b3a
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-18 10:46:43 +0530

    ADDED : EMP attendance check

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -9,10 +9,29 @@ package com.bridgelabz;
  */
 public class EmpWage {
 
+    public static final int IS_PRESENT = 1;
+
+    /**
+     * Purpose - checks whether an EMP is present or absent
+     */
+    public void attendanceCheck() {
+
+        int empRandomCheck = (int) (Math.random() * 2);
+
+        if (empRandomCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
+
     /**
      * Purpose - Execution of all methods
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        EmpWage attendanceCheck = new EmpWage();
+
+        attendanceCheck.attendanceCheck();
     }
 }
