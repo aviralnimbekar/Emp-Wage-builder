dc42193792bb5515a8d3e149aa53a0c9337b77d9
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-18 10:34:40 +0530

    ADDED : Welcome message

---
+++ src/com/bridgelabz/EmpWage.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - Employee Wage Computation program for multiple companies
+ *
+ * @author - Aviral N
+ * @version - 1.0
+ * date     - 18-08-2021
+ */
+public class EmpWage {
+
+    /**
+     * Purpose - Execution of all methods
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
