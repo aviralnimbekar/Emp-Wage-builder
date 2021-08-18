1f2ec37e578fbb01b11eb346eaa8dd09ffc2af73
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-18 11:41:42 +0530

    ADDED : Daily wage of EMP computation

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,24 +4,48 @@ package com.bridgelabz;
  * Purpose - Employee Wage Computation program for multiple companies
  *
  * @author - Aviral N
- * @version - 1.1
+ * @version - 1.2
  * date     - 18-08-2021
  */
 public class EmpWage {
 
     public static final int IS_PRESENT = 1;
+    public static final int FULL_TIME_WORKING_HR = 8;
 
     /**
      * Purpose - checks whether an EMP is present or absent
+     *
+     * @return - workingHrPerDay (8hr, 0hr)
      */
-    public void attendanceCheck() {
+    public static int attendanceCheck() {
 
+        int workingHrPerDay;
         int empRandomCheck = (int) (Math.random() * 2);
 
-        if (empRandomCheck == IS_PRESENT)
+        if (empRandomCheck == IS_PRESENT) {
             System.out.println("Employee is Present");
-        else
+            workingHrPerDay = FULL_TIME_WORKING_HR;
+        } else {
             System.out.println("Employee is Absent");
+            workingHrPerDay = 0;
+        }
+        return workingHrPerDay;
+    }
+
+    /**
+     * Purpose - Calculates daily wage if emp is  present
+     *
+     * @param workingHrPerDay working hrs of an EMP in a day (8, 0)
+     * @return dailyWage (160, 0)
+     */
+    public int dailyWage(int workingHrPerDay) {
+
+        int WAGE_PER_HRS = 20;
+
+        int dailyWage = WAGE_PER_HRS * workingHrPerDay;
+        System.out.println("Daily wage: " + dailyWage);
+
+        return dailyWage;
     }
 
     /**
@@ -30,8 +54,9 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        EmpWage attendanceCheck = new EmpWage();
+        EmpWage empWageCalculator = new EmpWage();
 
-        attendanceCheck.attendanceCheck();
+        int workingHrPerDay = empWageCalculator.attendanceCheck();
+        empWageCalculator.dailyWage(workingHrPerDay);
     }
 }
