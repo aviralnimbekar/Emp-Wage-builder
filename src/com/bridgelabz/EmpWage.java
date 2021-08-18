09528350fa55c1c7fed4b595f74b8880e4ed0180
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-18 11:56:06 +0530

    ADDED : Part time emp and daily wage

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -9,22 +9,27 @@ package com.bridgelabz;
  */
 public class EmpWage {
 
-    public static final int IS_PRESENT = 1;
-    public static final int FULL_TIME_WORKING_HR = 8;
+    public static final int IS_PRESENT = 1, IS_PART_TIME_PRESENT = 2;
+    public static final int FULL_TIME_WORKING_HR = 8, PART_TIME_WORKING_HR = 4;
 
     /**
-     * Purpose - checks whether an EMP is present or absent
+     * Purpose - checks whether an EMP is full time present, part-time present or absent
      *
-     * @return - workingHrPerDay (8hr, 0hr)
+     * @return - workingHrPerDay (8hr, 4hr, 0hr)
      */
-    public static int attendanceCheck() {
+    public int attendanceCheck() {
 
         int workingHrPerDay;
-        int empRandomCheck = (int) (Math.random() * 2);
+        int empRandomCheck = (int) (Math.random() * 3);
 
         if (empRandomCheck == IS_PRESENT) {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is full time Present");
             workingHrPerDay = FULL_TIME_WORKING_HR;
+
+        } else if (empRandomCheck == IS_PART_TIME_PRESENT) {
+            System.out.println("Employee is part time Present");
+            workingHrPerDay = PART_TIME_WORKING_HR;
+
         } else {
             System.out.println("Employee is Absent");
             workingHrPerDay = 0;
@@ -35,8 +40,8 @@ public class EmpWage {
     /**
      * Purpose - Calculates daily wage if emp is  present
      *
-     * @param workingHrPerDay working hrs of an EMP in a day (8, 0)
-     * @return dailyWage (160, 0)
+     * @param workingHrPerDay working hrs of an EMP in a day (8, 4, 0)
+     * @return dailyWage (160, 80, 0)
      */
     public int dailyWage(int workingHrPerDay) {
 
